3bbaa864f895e75ad8891c20af77d22080843b4f
akash-foodpanda <57343803+akash-foodpanda@users.noreply.github.com>
2021-02-26 23:04:14 +0800

    add Resolver protocol

---
+++ DependencyContainer/DependencyContainer/Resolver/Resolver.swift
@@ -0,0 +1,19 @@
+//
+//  Resolver.swift
+//  DependencyContainer
+//
+//  Created by Akash Rastogi on 26/2/21.
+//
+
+import Foundation
+
+protocol Resolver {
+  /// Resolves to an instance of type `Service` if instance/factory has already been registered.
+  func resolve<Service>(_ serviceType: Service.Type, name: String?) -> Service
+}
+
+extension Resolver {
+  func resolve<Service>(_ serviceType: Service.Type) -> Service {
+    return resolve(serviceType, name: nil)
+  }
+}
