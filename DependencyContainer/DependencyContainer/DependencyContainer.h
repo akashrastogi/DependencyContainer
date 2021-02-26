440755387585ec924898bcd7dfd4b470cf913c77
akash-foodpanda <57343803+akash-foodpanda@users.noreply.github.com>
2021-02-26 22:49:48 +0800

    add DependencyContainer framework

---
+++ DependencyContainer/DependencyContainer/DependencyContainer.h
@@ -0,0 +1,18 @@
+//
+//  DependencyContainer.h
+//  DependencyContainer
+//
+//  Created by Akash Rastogi on 26/2/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DependencyContainer.
+FOUNDATION_EXPORT double DependencyContainerVersionNumber;
+
+//! Project version string for DependencyContainer.
+FOUNDATION_EXPORT const unsigned char DependencyContainerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DependencyContainer/PublicHeader.h>
+
+
