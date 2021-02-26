91bff7126cb55f6238dd6ed1141ca5f7a21072b2
akash-foodpanda <57343803+akash-foodpanda@users.noreply.github.com>
2021-02-26 23:27:19 +0800

    add factory to resolve dependencies

---
+++ DependencyContainer/DependencyContainer/Factory/Factory.swift
@@ -0,0 +1,11 @@
+//
+//  Factory.swift
+//  DependencyContainer
+//
+//  Created by Akash Rastogi on 26/2/21.
+//
+
+import Foundation
+
+/// Define Factory to generate instances of the generic type `Service`
+typealias Factory<Service> = (Resolver) -> Service
