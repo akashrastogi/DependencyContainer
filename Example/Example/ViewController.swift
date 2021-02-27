fc7cbb20a24609f6e0f90e61e6d89c1c4179c020
akash-foodpanda <57343803+akash-foodpanda@users.noreply.github.com>
2021-02-27 12:17:33 +0800

    add example app

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Akash Rastogi on 27/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
